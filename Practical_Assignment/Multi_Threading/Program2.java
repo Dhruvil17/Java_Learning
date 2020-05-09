a199d230797962586354965732340f76a34ca082
jayBhagiya <jay.bict18@sot.pdpu.ac.in>
2020-05-09 14:41:27 +0530

    Made some changes.

--- Practical_Assignment/Multi_Threading/Program2.java
+++ Practical_Assignment/Multi_Threading/Program2.java
@@ -1,14 +1,46 @@
 package Practical_Assignment.Multi_Threading;
 
-class TrfficLight implements Runnable
+class TrafficLight
 {
-    public void run()
-    {
-        
+    public void lightOrder() throws InterruptedException {
+        while(true)
+        {
+            synchronized(this)
+            {
+                System.out.println("Stop");
+                System.out.println("RED : ON\n");
+                Thread.sleep(5000);
+
+                System.out.println("You may start your engine..");
+                System.out.println("YELLOW : ON\n");
+                Thread.sleep(3000);
+
+                System.out.println("Go..");
+                System.out.println("Gereen : ON\n");
+                Thread.sleep(5000);
+            }   
+        }
     }
 }
 
 
 public class Program2 {
-    
+    public static void main(String[] args) {
+
+        TrafficLight tf = new TrafficLight();
+
+        Thread t1 = new Thread(new Runnable(){
+        
+            @Override
+            public void run() {
+                try {
+                    tf.lightOrder();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "Light");
+
+        t1.start();
+    }
 }
